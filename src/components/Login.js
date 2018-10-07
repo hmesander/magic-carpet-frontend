db96cacc48c52687b08ccd9fee1bf25c68da7bc4
hmesander <haleymesander@gmail.com>
2018-10-07 10:07:35 -0600

    WIP: Updating README with screenshots and deleting bootstraped README

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = (props) => {
   return (
     <div className='container'>
-      <img className='logo' alt='magic-carpet-logo' src='/images/magic-carpet-logo.gif'/>
+      <img className='logo' alt='magic-carpet-logo' src='/public/images/app.gif'/>
       <button onClick={props.handleLogin} className='button'>Log In With Lyft</button>
     </div>
   );
